2bbd382900e8d2bdb2cb5eeb24bafb0e377aa2d3
danilorodrigues128 <50220862+danilorodrigues128@users.noreply.github.com>
2019-11-01 19:50:45 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+
+//-----[EXAMPLE]-----//
 #include <iostream>
 #include <stdlib.h>
 #include <stdio.h>
@@ -8,45 +10,33 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
+	NeuralNetwork Net;
+
+	Net.setLayers(4);
+
+	Net.setNeurons(0, 5);
+	Net.setNeurons(1, 3);
+	Net.setNeurons(2, 4);
+	Net.setNeurons(3, 3);
+
+	Net.config();
+	
 	srand(time(NULL));
+	for (int i = 0; i < Net.getNLayers() - 1; i++)
+		for (int j = 0; j < Net.getNNeurons(i); j++)
+			for (int k = 0; k < Net.getNNeurons(i + 1); k++)
+				Net.setWeight(i, j, k, (rand() % 2001) - 1000);
 
-	NeuralNetwork neuro;
+	Net.sendPulse(0, 200);
+	Net.sendPulse(1, 100);
+	Net.sendPulse(2, 200);
+	Net.sendPulse(3, 100);
+	Net.sendPulse(4, 200);
 
-	neuro.setLayers(4);
+	float* output = Net.outputNeurons();
 
-	neuro.setNeurons(0, 5);
-	neuro.setNeurons(1, 3);
-	neuro.setNeurons(2, 4);
-	neuro.setNeurons(3, 3);
-
-	neuro.config();
-	/*
-	for (int i = 0; i < neuro.getNLayers() - 1; i++)
-		for (int j = 0; j < neuro.getNNeurons(i); j++)
-			for (int k = 0; k < neuro.getNNeurons(i + 1); k++)
-				neuro.setWeight(i, j, k, (rand() % 2001) - 1000);*/
-
-	neuro.loadWeights("weights.txt");
-
-	neuro.sendPulse(0, 200);
-	neuro.sendPulse(1, 100);
-	neuro.sendPulse(2, 200);
-	neuro.sendPulse(3, 100);
-	neuro.sendPulse(4, 200);
-
-	float* output = neuro.outputNeurons();
-
-	for (int i = 0; i < neuro.getNNeurons(neuro.getNLayers() - 1); i++)
-	{
+	for (int i = 0; i < Net.getNNeurons(Net.getNLayers() - 1); i++)
 		std::cout << output[i] << std::endl;
-	}
-	std::cout << "----------------" << std::endl;
-	for (int i = 0; i < neuro.getNLayers() - 1; i++)
-		for (int j = 0; j < neuro.getNNeurons(i); j++)
-			for (int k = 0; k < neuro.getNNeurons(i + 1); k++)
-				std::cout << neuro.getWeight(i, j, k) << std::endl;
-
-	//neuro.saveWeights("weights.txt");
 
 	return 0;
 }
