0468ef86e601fa9f9a68c0369fc14558c8116b02
Danilo Rodrigues <50220862+danilorodrigues128@users.noreply.github.com>
2019-11-08 15:24:52 -0300

    Add files via upload

---
+++ ANNL/include/ANNL.h
@@ -0,0 +1,43 @@
+#ifndef ANNL_H
+#define ANNL_H
+
+#include <Layer.h>
+
+class NeuralNetwork
+{
+private:
+	int n_layers;
+	int* n_neurons;
+	bool ignoreNegatives;
+	Layer* layers;
+
+	void setSynapses();
+	void allocateWeights();
+	void allocatePulses();
+public:
+	NeuralNetwork(bool _ignoreNegatives);
+
+	void config();
+
+	void setLayers(int _Layers);
+
+	void setNeurons(int _Layer, int _nNeuron);
+
+	void setWeight(int _fromLayer, int _fromNeuron, int _toNeuron, int _Weight);
+
+	int getNNeurons(int _Layer);
+
+	int getNLayers();
+
+	int getWeight(int _fromLayer, int _fromNeuron, int _toNeuron);
+
+	void sendPulse(int _fromNeuron, float _Pulse);
+
+	void saveWeights(const char _Path[]);
+
+	void loadWeights(char* _Path);
+
+	float* outputNeurons();
+};
+
+#endif
